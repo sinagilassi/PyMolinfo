534254fbbd1bc9c85da5d074c2d9a5b978d2be22
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2024-08-17 14:29:06 -0400

    update app

--- molinfo/__init__.py
+++ molinfo/__init__.py
@@ -1,4 +1,4 @@
-from .app import main, __version__, g3d, g3d_by_inchi, check_functional_group, create_graph, compound
+from .app import main, __version__, g3d, g3d_by_inchi, check_functional_group, create_graph, compound, compound_by_cid, compound_by_inchi
 
 __all__ = ['main', '__version__', 'g3d',
-           'g3d_by_inchi', 'check_functional_group', 'create_graph', 'compound']
+           'g3d_by_inchi', 'check_functional_group', 'create_graph', 'compound', 'compound_by_cid', 'compound_by_inchi']
--- molinfo/config/setting.py
+++ molinfo/config/setting.py
@@ -6,6 +6,6 @@ packageName = 'Molecular Information'
 # package short name
 packageShortName = 'Molinfo'
 # package version
-__version__ = '1.2.0'
+__version__ = '1.3.0'
 # package description
 __description__ = 'A Python package for advanced molecular analysis by converting molecular structures into graph representations.'
