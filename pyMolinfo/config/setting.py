c02d2a99879f72a2a6de34ccb3b133619784c940
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2024-10-30 21:25:40 -0400

    ver 1.7.0

--- pyMolinfo/config/setting.py
+++ pyMolinfo/config/setting.py
@@ -6,6 +6,6 @@ packageName = 'Molecular Information'
 # package short name
 packageShortName = 'PyMolinfo'
 # package version
-__version__ = '1.6.0'
+__version__ = '1.7.0'
 # package description
 __description__ = 'A Python package for advanced molecular analysis by converting molecular structures into graph representations.'
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
 APP_NAME = 'PyMolinfo'
-VERSION = '1.6.0'
+VERSION = '1.7.0'
 DESCRIPTION = 'PyMolinfo provides comprehensive molecular information and analysis.'
 LONG_DESCRIPTION = 'PyMolinfo is a Python package designed for advanced molecular analysis by converting molecular structures into graph representations'
 
