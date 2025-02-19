c33e31da8ba53936a612ba514527658e33bab077
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2025-02-19 18:32:42 -0500

    new v1.7.1

--- pyMolinfo/config/setting.py
+++ pyMolinfo/config/setting.py
@@ -6,6 +6,8 @@ packageName = 'Molecular Information'
 # package short name
 packageShortName = 'PyMolinfo'
 # package version
-__version__ = '1.7.0'
+__version__ = '1.7.1'
 # package description
 __description__ = 'A Python package for advanced molecular analysis by converting molecular structures into graph representations.'
+# author
+__author__ = 'Sina Gilassi'
--- setup.py
+++ setup.py
@@ -8,7 +8,9 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
 APP_NAME = 'PyMolinfo'
-VERSION = '1.7.0'
+AUTHOR = 'Sina Gilassi'
+VERSION = '1.7.1'
+LICENSE = 'MIT'
 DESCRIPTION = 'PyMolinfo provides comprehensive molecular information and analysis.'
 LONG_DESCRIPTION = 'PyMolinfo is a Python package designed for advanced molecular analysis by converting molecular structures into graph representations'
 
@@ -16,13 +18,13 @@ LONG_DESCRIPTION = 'PyMolinfo is a Python package designed for advanced molecula
 setup(
     name=APP_NAME,
     version=VERSION,
-    author="Sina Gilassi",
+    author=AUTHOR,
     author_email="<sina.gilassi@gmail.com>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(exclude=['tests', '*.tests', '*.tests.*']),
-    license='MIT',
+    license=LICENSE,
     install_requires=['pandas', 'pillow', 'requests',
                       'urllib3', 'matplotlib', 'PubChemQuery', 'numpy', 'plotly', 'networkx'],
     keywords=['python', 'chemistry', 'chemistry-visualization',
@@ -35,5 +37,5 @@ setup(
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.10',
 )
