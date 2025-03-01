730d7a5a8c644ae56ae27edc998554f95cad4c88
sina.gilassi <sina.gilassi@gmail.com>
2025-02-28 22:45:46 -0500

    upgrade v1.8.3

--- pyMolinfo/config/setting.py
+++ pyMolinfo/config/setting.py
@@ -6,7 +6,7 @@ packageName = 'Molecular Information'
 # package short name
 packageShortName = 'PyMolinfo'
 # package version
-__version__ = '1.8.2'
+__version__ = '1.8.3'
 # package description
 __description__ = 'A Python package for advanced molecular analysis by converting molecular structures into graph representations.'
 # author
